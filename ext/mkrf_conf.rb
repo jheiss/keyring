69e9648ce57fb3d7c7f86175c6b75539e72ddfa0
Andi Bachmann <andi.bachmann@geo.uzh.ch>
2015-06-08 17:04:57 +0200

    . ready for platform-specific gem format

---
+++ ext/mkrf_conf.rb
@@ -0,0 +1,23 @@
+# http://en.wikibooks.org/wiki/Ruby_Programming/RubyGems#How_to_install_different_versions_of_gems_depending_on_which_version_of_ruby_the_installee_is_using
+
+require 'rubygems'
+require 'rubygems/command.rb'
+require 'rubygems/dependency_installer.rb' 
+begin
+  Gem::Command.build_args = ARGV
+rescue NoMethodError
+end 
+inst = Gem::DependencyInstaller.new
+begin
+  if RUBY_PLATFORM =~ /linux/
+    warn "*linux: installing gir_ffi-gnome_keyring..."
+    inst.install "gir_ffi-gnome_keyring", '~> 0.0.3'
+  end
+rescue
+  exit(1)
+end 
+
+# create dummy Rakefile to indicate success
+f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")
+f.write("task :default\n")
+f.close
--- keyring.gemspec
+++ keyring.gemspec
@@ -26,7 +26,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'mocha'
   spec.add_dependency 'slop', "< 4.0"
 
-  if RUBY_PLATFORM =~ /linux/
-    spec.add_dependency 'gir_ffi-gnome_keyring', '~> 0.0.3'
-  end
+  spec.extensions = %w[ext/mkrf_conf.rb]
+  # if RUBY_PLATFORM =~ /linux/
+  #   spec.add_dependency 'gir_ffi-gnome_keyring', '~> 0.0.3'
+  # end
 end
